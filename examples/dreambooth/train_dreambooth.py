f7c5300758c686a23555799a742575f937d1b77d
Eugene Liu <eugene.liu@intel.com>
2025-05-09 20:51:08 +0100

    Rename PixelDreamBoothDataset to DreamBoothDataset in training script

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -1186,7 +1186,7 @@ def main(args):
         pre_computed_class_prompt_encoder_hidden_states = None
 
     # Dataset and DataLoaders creation:
-    train_dataset = PixelDreamBoothDataset(
+    train_dataset = DreamBoothDataset(  # PixelDreamBoothDataset(
         instance_data_root=args.instance_data_dir,
         instance_prompt=args.instance_prompt,
         class_data_root=args.class_data_dir if args.with_prior_preservation else None,
